408dda3166157a2e4e265041436a1a08e45ea5c1
drazenp <drazenp86@gmail.com>
2018-02-24 00:41:31 +0000

    Fix html

--- footer.php
+++ footer.php
@@ -45,8 +45,8 @@
    <span class="popupContent" id="copyrightPopup">
      <h4>Copyright</h4>
       <p>All content on this web site is the property of Remote Pal.</p>
-      <p>Theme: Masonic by <a href="http://themegrill.com/themes/masonic" target="_blank" title="ThemeGrill" rel="author"><span>ThemeGrill</span></a><p>
-      <p>Header vector created by <a href="https://www.freepik.com/free-photos-vectors/background">Freepik</a>
+      <p>Theme: Masonic by <a href="http://themegrill.com/themes/masonic" target="_blank" title="ThemeGrill" rel="author"><span>ThemeGrill</span></a></p>
+      <p>Header vector created by <a href="https://www.freepik.com/free-photos-vectors/background">Freepik</a></p>
     </span>
    <div class="angled-background"></div>
 </footer>
